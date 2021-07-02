3ffcaaa05bd372a3d1912bbddabf522aa7cbece5
Tiago Nunes <ntiagon@gmail.com>
2021-07-02 19:25:37 -0300

    styling

--- src/components/Products/Product/Product.tsx
+++ src/components/Products/Product/Product.tsx
@@ -4,21 +4,28 @@ import {
   CardMedia,
   CardContent,
   CardActions,
-  Typography,
   IconButton,
   Typography,
 } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 
-const Product = (): JSX.Element => {
+const Product = ({ product }): JSX.Element => {
   return (
     <Card className={classes.root}>
       <CardMedia className={classes.media} image={} />
       <CardContent>
         <div className={classes.cardContent}>
-          <Typography variant="h5" gutterBottom></Typography>
+          <Typography variant="h5">{product.price}</Typography>
         </div>
+        <Typography variant="h2" color="textSecondary">
+          {product.description}
+        </Typography>
       </CardContent>
+      <CardActions className={classes.cardActions} disableSpacing>
+        <IconButton aria-label="add to cart">
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
     </Card>
   );
 };
