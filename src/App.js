f8149cb726ed554baa62b318367681bc0fc4d746
Tiago Nunes <ntiagon@gmail.com>
2021-07-03 11:40:00 -0300

    retrieving cart

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import { Products, Navbar } from './components';
 
 function App() {
   const [productCollection, setproductCollection] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
     setproductCollection(data);
+  };
+
+  const fetchCart = async () => {
+    const response = await commerce.cart.retrieve();
+    setCart(response);
   };
 
   useEffect(() => {
